ac2128b0fb53f906d4088f6ec52c6787426f2e06
Ekhtiar Hossain <ekhtiaroct2002@gmail.com>
2024-06-29 19:36:27 +0600

    First Tool

---
+++ domain_to_ip.py
@@ -0,0 +1,17 @@
+import socket
+import pyfiglet
+from termcolor import colored
+
+print(colored("*****************Domain To IP Converter*************************", "red"))
+print(colored("*****************Domain To IP Converter*************************", "green"))
+
+
+banner = colored(pyfiglet.figlet_format("IP CONVERTER"), "red")
+
+print(banner)
+
+domain_name = input("Enter Domain Name: ")
+
+ip = socket.gethostbyname(domain_name)
+
+print("IP for {} is:{}".format(domain_name, ip))
