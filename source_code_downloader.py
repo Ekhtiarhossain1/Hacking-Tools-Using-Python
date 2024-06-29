92a906a008bea1b66b61dc862b1fd184b32cecec
Ekhtiar Hossain <ekhtiaroct2002@gmail.com>
2024-06-30 00:34:41 +0600

    Second Tool

---
+++ source_code_downloader.py
@@ -0,0 +1,18 @@
+import urllib.request as ur
+import pyfiglet
+from termcolor import colored
+
+print(colored("*****************Website Source Code Downloader*************************", "red"))
+print(colored("*****************Website Source Code Downloader*************************", "green"))
+
+banner = colored(pyfiglet.figlet_format("Source Code"), "red")
+banner2 = colored(pyfiglet.figlet_format("Downloader"), "green")
+print(banner, banner2)
+
+
+url = input("Enter Target Website URL(ex: https://www.google.com): ")
+
+source = ur.urlopen(url)
+source_code = source.read()
+
+print(source_code)
